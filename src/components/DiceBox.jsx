3f034320df06da525e2d8d80a3bb79480ee53abe
Olivier-Masabo <masaboolivier21@gmail.com>
2026-02-22 22:51:03 +0200

    setting up dice game page

---
+++ src/components/DiceBox.jsx
@@ -0,0 +1,11 @@
+export default function Dicebox(props){
+
+    return(
+        
+        <div>
+            <button className="font-semibold text-xl rounded-xl shadow-lg shadow-gray-400 w-15 h-15 border border-gray-300">{props.number}</button>
+           
+        </div>
+    
+    )
+}
